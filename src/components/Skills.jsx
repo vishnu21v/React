444a60565ed6d91cf730ee43ebed0fa47707742f
vishnu21v <vishnuvaitheeswaran2002@gmail.com>
2025-12-16 12:44:54 +1100

    undone changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,37 +1,32 @@
 import React from "react";
-import "./Skills.css";
 
 const skills = [
-  { name: "GitHub", icon: "fa-github" },
-  { name: "Flickr", icon: "fa-flickr" },
-  { name: "HTML", icon: "fa-html5" },
-  { name: "CSS", icon: "fa-css3-alt" },
-  { name: "JavaScript", icon: "fa-js" },
-  { name: "Python", icon: "fa-python" },
-  { name: "Java", icon: "fa-java" },
-  { name: "Figma", icon: "fa-figma" },
-  { name: "Git", icon: "fa-git-alt" },
+  "React",
+  "JavaScript",
+  "Python",
+  "Flask",
+  "Machine Learning basics",
+  "Git / GitHub",
+  "HTML & CSS",
+  "Data Cleaning",
+  "NLP fundamentals",
+  "Data mining",
 ];
 
 const Skills = () => {
   return (
-    <section className="sec edu" id="skills">
-      <div className="container skills">
+    <section id="skills" className="section">
+      <div className="Skills">
         <h2 className="section-title">Skills</h2>
-
-        <div className="scroller" data-animated="true">
-          <ul className="scroller_content">
-            {[...skills, ...skills].map((skill, index) => (
-              <li key={index} className="skill-item">
-                <div>
-                  <i className={`fa-brands ${skill.icon}`}></i>
-                </div>
-                <span>{skill.name}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="skills-grid">
+        {skills.map((s) => (
+          <div key={s} className="skill-pill">
+            {s}
+          </div>
+        ))}
       </div>
+      </div>
+      
     </section>
   );
 };
