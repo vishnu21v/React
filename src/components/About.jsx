ae74a7bab5e2ba7b539a3fc250962a81e9764435
vishnu21v <vishnuvaitheeswaran2002@gmail.com>
2025-12-16 15:46:16 +1100

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,10 @@
 import React from "react";
 
-
-
-
-import { useEffect } from "react";
-
-useEffect(() => {
-  const elements = document.querySelectorAll(".reveal");
-
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        setTimeout(() => {
-          entry.target.classList.add("active");
-        }, 3000); // 👈 3 seconds delay
-      }
-    });
-  }, { threshold: 0.2 });
-
-  elements.forEach(el => observer.observe(el));
-}, []);
-
-
 const About = () => {
   return (
-    <section id="about" className="section reveal">
-      <div className=" About">
-          <h2 className="section-title">About</h2>
+    <section id="about" className="section">
+      <div className="About">
+        <h2 className="section-title">About</h2>
         <p>
           I'm an AI / Data / Web enthusiast currently studying and building
           projects in React, Python, ML, and academic assignments (classification,
