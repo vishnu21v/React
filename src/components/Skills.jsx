3aefbe6fcefea411dbd03d8b258f88e4d523d6cd
vishnu21v <vishnuvaitheeswaran2002@gmail.com>
2025-12-16 13:41:34 +1100

    designing the skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,15 +12,25 @@ import {
   faGitAlt,
 } from "@fortawesome/free-brands-svg-icons";
 
-
+const Skills = () => {
+  const skills = [
+    { name: "Github", icon: faGithub },
+    { name: "Flickr", icon: faFlickr },
+    { name: "HTML", icon: faHtml5 },
+    { name: "CSS", icon: faCss3Alt },
+    { name: "JS", icon: faJs },
+    { name: "Python", icon: faPython },
+    { name: "Java", icon: faJava },
+    { name: "Figma", icon: faFigma },
+    { name: "Git", icon: faGitAlt },
+  ];
 
   return (
     <section className="section" id="skills">
-      <div className="Projects">
-        <h2 className="section-title">Skills</h2>
-      </div>
+      <h2 className="section-title">Skills</h2>
+
       <div className="container skills">
-        <div className="scroller" data-animated="true">
+        <div className="scroller">
           <ul className="scroller_content">
             {[...skills, ...skills].map((skill, index) => (
               <li key={index}>
@@ -33,6 +43,6 @@ import {
       </div>
     </section>
   );
-
+};
 
 export default Skills;
