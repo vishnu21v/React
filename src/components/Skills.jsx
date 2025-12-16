30bd2b57c854d9e651b9389b24587eff2c865c65
vishnu21v <vishnuvaitheeswaran2002@gmail.com>
2025-12-16 12:56:27 +1100

    updated thee skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,56 +11,43 @@ const Skills = () => {
               <div><i className="fa-brands fa-github"></i></div>
               Github
             </li>
-
             <li>
               <div><i className="fa-brands fa-flickr"></i></div>
               Flickr
             </li>
-
             <li>
               <div><i className="fa-brands fa-html5"></i></div>
               HTML
             </li>
-
             <li>
               <div><i className="fa-brands fa-css3-alt"></i></div>
               CSS
             </li>
-
             <li>
               <div><i className="fa-brands fa-js"></i></div>
               JS
             </li>
-
             <li>
               <div><i className="fa-brands fa-python"></i></div>
               Python
             </li>
-
             <li>
               <div><i className="fa-brands fa-java"></i></div>
               Java
             </li>
-
             <li>
               <div><i className="fa-brands fa-figma"></i></div>
               Figma
             </li>
-
             <li>
               <div><i className="fa-brands fa-git-alt"></i></div>
               Git
             </li>
 
-            {/* duplicate items for infinite scroll */}
+            {/* duplicate for infinite scroll */}
             <li>
               <div><i className="fa-brands fa-github"></i></div>
               Github
-            </li>
-
-            <li>
-              <div><i className="fa-brands fa-css3-alt"></i></div>
-              CSS
             </li>
           </ul>
         </div>
