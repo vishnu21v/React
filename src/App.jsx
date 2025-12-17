9611562fc30762f39062007eec138017292a1a80
vishnu21v <vishnuvaitheeswaran2002@gmail.com>
2025-12-17 23:33:51 +1100

    updated animation to every section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -6,20 +7,22 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import useViewportVisibility from "./hooks/useViewportVisibility";
-
 
 function App() {
   useEffect(() => {
     const sections = document.querySelectorAll(".section");
+
+    sections.forEach(section => section.classList.add("hidden"));
 
     const observer = new IntersectionObserver(
       entries => {
         entries.forEach(entry => {
           if (entry.isIntersecting) {
             entry.target.classList.add("visible");
+            entry.target.classList.remove("hidden");
           } else {
             entry.target.classList.remove("visible");
+            entry.target.classList.add("hidden");
           }
         });
       },
