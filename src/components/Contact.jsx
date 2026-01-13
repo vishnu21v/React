1fe898e47a129683ad6112991febed4866b0f3dc
Vishnu V <vishnuvaitheeswaran2002@gmail.com>
2026-01-13 09:02:12 +0000

    adjusting footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,8 +12,8 @@ const Contact = ({ className }) => {
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#skills">Skills</a></li>
-            <li><a href="#Projects">Projects</a></li>
-            <li><a href="#Certificates">Certificates</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#certificates">Certificates</a></li>
           </div>
           <div className="contact-buttons">
             <a
