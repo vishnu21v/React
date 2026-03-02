aa964286215866cf9b33d09c6fac979dd6bc612a
Vishnu V <vishnuvaitheeswaran2002@gmail.com>
2026-03-02 04:00:10 +0000

    amending content in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,7 +21,7 @@ const textVariants = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 11, ease: "easeOut" },
+    transition: { duration: 0.5, ease: "easeOut" },
   },
 };
 
